f5d1715af310d9c220baa01ef312fb3652661875
Enock MUKOKO MAYA <emukoko@planetworkint.com>
2024-09-17 22:27:19 +0200

    fix settings

--- MSPR4/settings.py
+++ MSPR4/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_ROOT = '/var/www/Api_Customers1/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
