bf04bbccbca093fd3963d3f175f1361fee3374ae
Enock MUKOKO MAYA <emukoko@planetworkint.com>
2024-09-23 11:03:39 +0200

    add customers url

--- customers/urls.py
+++ customers/urls.py
@@ -2,8 +2,12 @@
 from .views import CustomerViewSet
 from django.urls import path
 from .views import CustomerViewSet, CustomerOrdersView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'customers', CustomerViewSet, basename='customer')
 urlpatterns = [
-    path('customers/', CustomerViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('', include(router.urls)),
     path('customers/<int:customer_id>/orders/', CustomerOrdersView.as_view(), name='customer-orders'),
 ]
